9918c0eac2ca485f69ba2668853a85ffd6474590
Drawcard <git@drawcard.com.au>
2017-12-20 17:11:26 +1030

    feat(rb): Add Photomill

---
+++ Casks/photomill.rb
@@ -0,0 +1,10 @@
+cask 'photomill' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://overmacs.com/photomill/downloads/PhotoMillTrial.dmg'
+  name 'PhotoMill'
+  homepage 'http://overmacs.com/?p=photomill'
+
+  app 'PhotoMill X.app'
+end
